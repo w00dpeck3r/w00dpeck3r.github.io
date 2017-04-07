892d13779589b8953e8ed8c7db6f9bf6ebdfdf66
uoo38 <plus.wisteria38@gmail.com>
2017-04-07 15:24:37 +0900

    add project icons

---
+++ project.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<div id="content" class="container">
+  <div id="links">
+    <h2><a href="<?php echo site_url(); ?>/#top">ホーム</a> > <a href="#">PROJECT - ゲームアプリ</a></h2>
+  </div>
+  <div class="article beige-background">
+    <h3>活動内容</h3>
+  </div>
+  <div class="article white-background">
+    <h3>活動日</h3>
+  </div>
+  <div class="article beige-background">
+    <h3>ギャラリー</h3>
+  </div>
+</div> <!-- content ends here -->
+
+<div id="pagetop">
+  <a href="#top"><img src="./images/pagetop.png" alt="jump to top of this page"></a>
+</div>
+
+<?php get_footer(); ?>
