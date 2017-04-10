7eed53023fb6f8d0ea5f89f4ce67bdb0fef07c27
uoo38 <plus.wisteria38@gmail.com>
2017-04-10 09:46:19 +0900

    add footer link

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     </div>
 
     <div id="footer" class="container">
-      <p><a href="#">お問い合わせ</a> | <a href="#">サイトマップ</a></p>
+      <p><a href="goo.gl/PJYu4y">お問い合わせ</a> | <a href="#">サイトマップ</a></p>
       <p>Copyright (c) 2017 Copyright WoodPecker All Rights Reserved.</p>
     </div> <!-- footer ends here -->
     <?php wp_footer(); ?>
